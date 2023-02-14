73b77eb8afbac122aa2bdf909ce29df12d15464b
Gauravbaghel_09 <95128634+gauravbaghel09@users.noreply.github.com>
2023-02-15 04:41:42 +0530

    Update union.cpp

--- union.cpp
+++ union.cpp
@@ -27,8 +27,40 @@ int main()
     // return 0;
 
 //second method.
+    
+    // array 1
+//     int arr1[] = {1, 3, 5, 7, 9};
+//     int n1 = 5;
 
+//     // array 2
+//     int arr2[] = {2, 4, 6, 8};
+//     int n2 = 4;
 
+//     // creating a vector
+//     vector<int> ans;
+
+//     // copying elements of arr1 to ans vector
+//     for (int i = 0; i < n1; i++)
+//     {
+//         ans.push_back(arr1[i]);
+//     }
+
+//     // copying elements of arr2 to ans vector
+//     for (int i = 0; i < n2; i++)
+//     {
+//         ans.push_back(arr2[i]);
+//     }
+
+//     // printing vector
+//     for (int i = 0; i < ans.size(); i++)
+//     {
+//         cout << ans[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+//third method.
+    
 int arr1[] = {1, 2, 3, 4, 5};
 int arr2[] = {2, 3, 8};
 int n = sizeof(arr1) / sizeof(int);
